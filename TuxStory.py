8499dd8ff5efbbebe40e5040844e3939bc9daf4f
vnbq <foxysroadtech@gmail.com>
2024-09-12 16:17:56 -0500

    Add files via upload

---
+++ TuxStory.py
@@ -0,0 +1,95 @@
+integer = 0
+item = 'hello'
+Item = 'world'
+
+print(item, Item)
+
+item = 'Welcome to the'
+Item = 'Daisy fields!'
+
+print(item, Item)
+
+import time
+
+time.sleep(2)
+
+item = 'I will go to'
+Item = 'the Hay Fields!'
+
+print(item, Item)
+
+import time
+
+time.sleep(10)
+
+item = 'I have arrived at'
+Item = 'the Hay fields!'
+
+print(item, Item)
+
+item = 'So,'
+Item = 'anyways I am Tux, a Penguin!'
+
+print(item, Item)
+
+print('I am also an explorer!')
+
+import time
+time.sleep(2.5)
+
+print('Now I will continue exploring!')
+
+import time
+time.sleep(1.5)
+
+item = 'I will go'
+Item = 'To the Red Moon Desert Now!'
+
+print(item, Item)
+
+import time 
+time.sleep(2.5)
+
+print('Now I have arrived!')
+
+item = 'So,'
+Item = 'I will go to Azul.JPG now'
+
+print(item, Item)
+
+import time
+time.sleep(2)
+
+print('But before I go to Azul.JPG')
+print('I need to get a boat!')
+
+import time
+time.sleep(2)
+
+item = 'So I have arrived at'
+Item = 'Atumn.jpg, and now I will get some wood'
+
+print(item, Item)
+
+import time
+time.sleep(3)
+
+print('So, now that I have gotten wood, I can now go to Azul.jpg!')
+
+import time
+time.sleep(10)
+
+print('Now I arrived at Azul! Thanks for listening!')
+
+import time
+time.sleep(10)
+
+item = 'Good night.'
+Item = 'Now I will sleep!'
+
+print(item, Item)
+
+import time
+time.sleep(20)
+
+print('Good morning!')
